955657a63fe1f8f002148811e4c74c82a3b011b8
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-09 21:24:27 +0300

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by YASIN AKCA on 9.04.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
